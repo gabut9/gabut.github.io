bb85f6473082c882e07b209ac4dc4cc9f14ee46d
gabut9 <maladin0210@gmail.com>
2021-03-05 07:55:32 -0800

    first commit

---
+++ project1.js
@@ -0,0 +1,81 @@
+alert('assalamualaikum ');
+alert('kalian kangen aku gak??');
+alert('ih gamau');
+alert('gak suka gelayy');
+alert('wellcome tuh my script');
+alert('aku ada pantun nih');
+alert('mang tatang naik kereta');
+alert('dari surabaya menuju surakarta');
+alert('selamat datang, salam sejahtera');
+alert('semoga bahagia selalu menyerta');
+alert('aamiin')
+
+	alert('aku ada pantun lagi');
+
+
+var tanya = true;
+while ( tanya ){
+var kedua = prompt('mau pantun tentang apa nih?? \n (pilihan: lucu,gombal,nasihat)');
+switch( kedua ) {
+	case 'lucu':
+	alert('ikan bandeng, ikan mujaer');
+	alert('ikan lele, ikan patin');
+	alert('ya itu nama nama ikan');
+	alert('hhahahha');
+	break;
+	case 'gombal' :
+	alert('minum jamu rasa pepaya');
+	alert('cintaku padamu tiada duanya');
+	alert('hhahahaha:v canda cinta><');
+	break;
+	case 'nasihat' :
+	alert('ke pasar beli celana');
+	alert('jangan lupa beli lemari');
+	alert('jika ingin terhindar dari corona');
+	alert('makanlah asupan yang bergizi');
+	alert(':)');
+	alert('semoga kita selalu dalam lindungan allah');
+	alert('aamiin')
+	break;
+	default :
+	alert('yah maaf, pantun yang anda ingin kan tidak tersedia');
+	break;
+}
+tanya = confirm('mau baca pantun lagi??');
+}
+
+	alert('yudh klo gk mau baca pantun lagi mah:)');
+	alert('tapi aku ada tebak tebak an nih buat kamu');
+	alert('coba deh kamu liat telapak tangan kanan kamu');
+	alert('tangan kanan');
+	alert('nah trus,posisinya itu kamu bisa ngeliat kuku');
+	alert('bahasa lainnya itu liat bagian luar tangan');
+	alert('udh kan?');
+
+var jawaban = true;
+while ( jawaban ){
+var ketiga = prompt('nah klo udh,sebelah kanan dari jari tengah kamu itu jari apa?');
+if ( ketiga == 'jari') {
+	alert('yah kok tau jawabannya:)');
+var keempat = prompt('alasannya apa cba??');
+if (keempat == 'ya karena manisnya udh diambil kamu^^') {
+
+} else {
+	alert('salah');
+}
+	 
+} else  {
+	alert('salah');
+}
+jawaban = confirm('mau jawab ulang?')
+}
+
+	alert('nyerah nih?');
+	alert('oke deh');
+	alert('jawabannya yaitu :');
+	alert('jari');
+	alert('iya jarinya aja');
+	alert('ya karena manisnya udh diambil kamu^^');
+	alert('hhahahaha:v');
+	alert('udh lah pegel');
+	alert('selamat istirahat:)');
